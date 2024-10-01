3458a29a1bd72ecf428999657499eeba56efa163
Alerugg <134484194+Alerugg@users.noreply.github.com>
2024-10-01 20:34:41 +0000

    queda pendiente arreglar el condicional donde se crea el usuario si no exite y listo

--- src/js/component/fetch.jsx
+++ src/js/component/fetch.jsx
@@ -1,300 +1,309 @@
 import React, { useState, useEffect } from "react";
-import { BsFillEraserFill } from "react-icons/bs";
-
-
 
 /// pendientes: arreglar que se vean las viñetas quitando el flex pero acomodando que queden los iconos del lado derecho sin moverse...
-/// preguntas a irio y gaston: como uso position relative y absolute para los iconos 
-
+/// preguntas a irio y gaston: como uso position relative y absolute para los iconos
 
 const ToDoList = () => {
-
-    const [tasks, setTasks] = useState([])
-    const [inputValue, setInputValue] = useState("")
-    const [error, setError] = useState(false)
-    const [error2, setError2] = useState(false)
-
-    const user = "AleRugg"
-    const getUserUrl = "https://playground.4geeks.com/todo/users/AleRugg"
-
-   
+  const [tasks, setTasks] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [error, setError] = useState(false);
+  const [error2, setError2] = useState(false);
+  const [allUsers, setAllUsers] = useState([]); // aqui almacenaramos los datos de los usuarios en base de datos para crear usuarios y no puedas meter uno que ya existe y no de error el post
+  const [user, setUser] = ([])
 
 
-    const inputHandler = (e) => {
+  const user1 = "AleRugg";
 
-        setInputValue(e.target.value)
+  const inputHandler = (e) => {
+    setInputValue(e.target.value);
+  };
+
+
+  const handlerKeyDown = (e) => {
+    if (e.key === "Enter" && inputValue !== "") {
+      createToDo();
+      setInputValue(""); // Limpia el input despues de la interaccion
     }
+  };
 
-    const handlerKeyDown = (e) => {
+  //GETS
 
-        if (e.key === "Enter" && inputValue.trim() !== "") {
+  const getUserTasks = () => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+    fetch(`https://playground.4geeks.com/todo/users/${user1}`, requestOptions)
+      .then((response) => response.json())
+      .then((result) => setTasks(result.todos))
+      .catch((error) => console.error(error));
+  };
 
-            setTasks([...tasks, inputValue])
-            setInputValue(""); // Limpia el input despues de la interaccion
-        }
+  const getUsers = () => {
 
-
-    }
-
-    useEffect(()=> {
-        if(tasks.length == 10) {
-            setError(true)
-           // alerta de maximas tareas diarias (10)
-        }
-
-        else setError(false)
-    },[tasks])
-
-    let maxLength = 100;
-
-    useEffect(() => {
-        if( inputValue.length == maxLength) { // alerta de maximos caracateres
-            setError2(true)
-        }
-        else {setError2(false)}
-        
-       
-    }, [inputValue])
-
-    const deleteTask = (index) => {
-        const newTasks = tasks.filter((_, i) => i !== index); // borrar articulos de la lista 
-        setTasks(newTasks)}; 
-
-
-      useEffect(() => {
-
-        fetch(getUserUrl)
-        .then(response => response.json()) 
-        .then(data => {
-          if (data.todos) { setTasks(data.todos)}
-        })   // aqui estamos obteniendo la informacion del usuario desde el servidor, en este caso son tareas por loq ue asignamos las tareas a setTasks
-        .catch(error => {
-          console.error(`Ha habido un error:${error}`)
-        })
-
-      }, [])
-
-      
-
-
-
-    return (
-
-        <div className="container toDoWrapper">
-
-            <div className="card bg-dark cont" >
-                <div className=" d-flex justify-content-center align-items-center mt-2">
-                    <div className="maxAlerts">
-                    <input
-                        type="text"
-                        placeholder="Añade tu tarea y pulsa Enter para añadirla a la lista..."
-                        value={inputValue}
-                        onChange={inputHandler}
-                        onKeyDown={handlerKeyDown}
-                        disabled={tasks.length >= 10}
-                        maxLength={100}
-                          
-                    />
-                    
-                    
-                    { error && <span className=" mxLength">Solo puedes añadir 10 tareas a tu lista!</span>}
-                    { error2 && <span className=" maxArticles">Tus tareas pueden tener como maximos 100 caracteres, lo siento!</span>}
-                    </div>
-
-
-                </div>
-                <h4 className="title">Lista de tareas por hacer:</h4>
-
-                <div className="card-body">
-                    <ul>
-                        {tasks.map((task, index) => {
-
-                            return (
-
-                                <li className="text-white liGenerado"
-                                    value={task.label}
-                                    key={index}
-                                    
-                                >
-                                    {task.label}                                    
-                                        <BsFillEraserFill className="eraserIcon" onClick={() => deleteTask(index)} />                                    
-                                </li>
-
-                            )
-                        })}
-                    </ul>
-                    <div className="card.footer text-white footer">Llevas {tasks.length} / 10 tareas agregadas a tu lista.</div>
-                </div>
-            </div>
-        </div>
+    const requestOptions = {
+      method: "GET", // este GET nos trae todos los usuarios existentes en la base de datos
+    };
+    fetch(
+      "https://playground.4geeks.com/todo/users?offset=0&limit=100",
+      requestOptions
     )
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.users) {
+          const userNames = result.users.map((user) => user.name); // con esto obtenemos los nombres para poder hacer comparaciones de si existen o no
+          setAllUsers(userNames, result)
+          console.log(allUsers);   
+        }
+      })
+      .catch((error) => console.error(error));
+
+  }
+
+ 
+
+  // DELETES
+
+  const deleteTask = (id) => {
+    const newTasks = tasks.filter((task) => task.id !== id); // borrar articulos de la lista
+    deleteToDo(id);
+    console.log(id);
+    setTasks(newTasks);
+  };
+
+  const deleteAllTasks = () => {
+    tasks.forEach((task) => {
+      deleteToDo(task.id);
+    });
+  };
+
+  const deleteToDo = (id) => {
+    const requestOptions = {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+    };
+
+    fetch(`https://playground.4geeks.com/todo/todos/${id}`, requestOptions)
+      .then((response) => {
+        if (response.ok) {
+          getUserTasks();
+        }
+        response.json();
+      })
+      .then((result) => console.log(result))
+      .catch((error) => console.error(error));
+  };
+
+  const createUser = () => {
+    fetch("https://playground.4geeks.com/todo/users/AleRugg", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
+  };
+
+  //comparativa para que cree el usuario al iniciar la app // crea el usuario pero no esta ejecutando la condicion por el momento 
+
+  const findAName = () => {
+    const existingUser = allUsers.find((user) => user === user1); // Accedes al estado allUsers directamente
+
+    if (allUsers && existingUser) {
+      console.log("Ya Creado");
+    } else {
+      createUser();
+      console.log(allUsers);
+      console.log(user);
+    }
+  };
+
+  // SOME CONDITIONS
+
+  const condition1 = () => {
+    if (tasks.length == 10) {
+  setError(true);
+  // alerta de maximas tareas diarias (10)
+} else setError(false);
 }
 
-export { ToDoList }
+const condition2 = () => {
+if (inputValue.length == maxLength) {
+  // alerta de maximos caracateres
+  setError2(true);
+} else {
+  setError2(false);
+}
 
 
+  useEffect(() => {
+    getUserTasks();
+  }, []);
 
+  useEffect(() => {
+getUsers()
+  }, []);
 
+  useEffect(() => {
+    condition1()
+  }, [tasks]);
 
+  let maxLength = 100;
 
-
-
-
-// import React, { useState, useEffect } from "react";
-// import { BsFillEraserFill } from "react-icons/bs";
-
-// /// pendientes: arreglar que se vean las viñetas quitando el flex pero acomodando que queden los iconos del lado derecho sin moverse...
-// /// preguntas a irio y gaston: como uso position relative y absolute para los iconos
-
-// const ToDoList = () => {
-//   const [tasks, setTasks] = useState([]);
-//   const [inputValue, setInputValue] = useState("");
-//   const [error, setError] = useState(false);
-//   const [error2, setError2] = useState(false);
-
-//   const user = "AleRugg";
-
-//   const getUserUrl = `https://playground.4geeks.com/todo/users/${user}`;
-//   const postUserUrl = `https://playground.4geeks.com/todo/users/${user}`;
-//   const putUserUrl = "";
-//   const deleteUserUrl = "";
-
-//   const getToDoUrl = "";
-//   const postToDoUrl = `https://playground.4geeks.com/todo/todos/${user}`;
-//   const putToDoUrl = "";
-//   const deleteToDoUrl = "";
-
-
-//   const inputHandler = (e) => {
-//     setInputValue(e.target.value);
-//   };
-
-//   useEffect(() => {
-//     if (tasks.length == 10) {
-//       setError(true);
-//       // alerta de maximas tareas diarias (10)
-//     } else setError(false);
-//   }, [tasks]);
-
-//   useEffect(() => {
-//     let maxLength = 100;
-//     if (inputValue.length == maxLength) {
-//       // alerta de maximos caracateres
-//       setError2(true);
-//     } else {
-//       setError2(false);
-//     }
-//   }, [inputValue]);
-
-//   const deleteTask = (id) => {
-//     // const newTasks = tasks.filter((_, id) => id !== id); // borrar articulos de la liksta
-//     // setTasks(newTasks);
-//     // paso 1: deberiamos hacer fecth a delete
-//     // que necesitamos? url y el id 
-//     fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
-//         method: "DELETE"
-//     })
-//     .then(() => setTasks(tasks.filter((task) => id !== task.id ))) // esto devuelve un objeto filtrado ver funcion filter mejor
-    
-    
-//   };
+  useEffect(() => {
+    condition2()
+  }, [inputValue]);
+}
 
   
+  
 
-//   const handlerKeyDown = (e) => {
-//     if (e.key === "Enter" && inputValue !== "") {
-//       addToDo({
-//         label: inputValue,
-//         is_done: false,
-//       });
-//     }
-//   };
+  useEffect(()=>{
+    findAName()
+  },[])
 
-//   useEffect(() => {
-//     fetch(getUserUrl)
-//       .then((response) => response.json()) // con esto obtenemos la informacion del usuario almacenado/ creado en el servidor de la app
-//       .then((data) => {
-//         if (data.todos) {
-//           setTasks(data.todos);
-//         }
-//       });
+  useEffect(() => {
+    console.log(allUsers);
+    console.log(tasks);
+  }, [allUsers, tasks]);
 
-//     // utilizar el todos porque es el nombre del array de objetos que trae el metodo desde la API
-//   }, []);
+  const createToDo = () => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
 
-//   const addToDo = (body) => {
-//     fetch(postToDoUrl, {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify(body),
-//     })
-//       .then((response) => response.json())
-//       .then((data) => setTasks([...tasks, data]))
-//       .then(() => setInputValue(""))
-//       .catch((error) => console.error(error));
-//   };
+    const raw = JSON.stringify({
+      label: inputValue,
+      is_done: false,
+    });
 
-//   return (
-//     <div className="container toDoWrapper">
-//       <div className="card bg-dark cont">
-//         <div className=" d-flex justify-content-center align-items-center mt-2">
-//           <div className="maxAlerts">
-//             <input
-//               type="text"
-//               placeholder="Añade tu tarea y pulsa Enter para añadirla a la lista..."
-//               value={inputValue}
-//               onChange={inputHandler}
-//               onKeyDown={handlerKeyDown}
-//               disabled={tasks.length >= 10}
-//               maxLength={100}
-//             />
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+    };
 
-//             {error && (
-//               <span className=" mxLength">
-//                 Solo puedes añadir 10 tareas a tu lista!
-//               </span>
-//             )}
-//             {error2 && (
-//               <span className=" maxArticles">
-//                 Tus tareas pueden tener como maximos 100 caracteres, lo siento!
-//               </span>
-//             )}
-//           </div>
-//         </div>
-//         <h4 className="title">Lista de tareas por hacer:</h4>
+    fetch("https://playground.4geeks.com/todo/todos/AleRugg", requestOptions)
+      .then((response) => response.json())
+      .then((result) => setTasks([...tasks, result]))
+      .catch((error) => console.error(error));
+  };
 
-//         <div className="card-body">
-//           <ul>
-//             {tasks.map((task, index) => {
-//               return (
-//                 <li
-//                   className="text-white liGenerado"
-//                   // value={task.label}
-//                   key={index}
-//                 >
-//                   {task.label}
-//                   <BsFillEraserFill
-//                     className="eraserIcon"
-//                     onClick={() => deleteTask(task.id)}
-//                   />
-//                   {task.is_done === false ? "sin hacer" : "ya hecho"}
-//                 </li>
-//               );
-//             })}
-//           </ul>
-//           <div className="card.footer text-white footer">
-//             Llevas {tasks.length} / 10 tareas agregadas a tu lista.
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
+  const doneUpdate = (task) => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    task.is_done = !task.is_done;
+    const taskMap = tasks.map((taskItem) => {
+      if (taskItem.id === task.id) {
+        return task;
+      }
+      return taskItem;
+    });
+    setTasks(taskMap);
+    const raw = JSON.stringify(task);
 
-// export { ToDoList };
+    const requestOptions = {
+      method: "PUT",
+      headers: myHeaders,
+      body: raw,
+    };
 
-// EXTRA agregar un boton de finalizar tarea, comn put y cambiar el isdone de falso a true
+    fetch(`https://playground.4geeks.com/todo/todos/${task.id}`, requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.error(error));
+  };
 
+  return (
+    <div className="container toDoWrapper">
+      <div className="card bg-dark cont">
+        <div className=" d-flex justify-content-center align-items-center mt-2">
+          <div className="maxAlerts">
+            <input
+              className="inputResponsive"
+              type="text"
+              placeholder="Añade tu tarea y pulsa Enter para añadirla a la lista..."
+              value={inputValue}
+              onChange={inputHandler}
+              onKeyDown={handlerKeyDown}
+              disabled={tasks.length >= 10}
+              maxLength={100}
+            />
+            {error && (
+              <span className=" mxLength">
+                Solo puedes añadir 10 tareas a tu lista!
+              </span>
+            )}
+            {error2 && (
+              <span className=" maxArticles">
+                Tus tareas pueden tener como maximos 100 caracteres, lo siento!
+              </span>
+            )}
+          </div>
+        </div>
+        <h4 className="title">Lista de tareas por hacer:</h4>
+        <div className="card-body">
+          <ul>
+            {tasks.map((task, index) => {
+              return (
+                <li
+                  className="text-white liGenerado"
+                  value={task.label}
+                  key={index}
+                >
+                  {task.label}
+                  <div
+                    className="btn-group md-0"
+                    role="group"
+                    aria-label="Basic mixed styles example"
+                  >
+                    <button
+                      type="button"
+                      id="checkButton"
+                      className={`btn  ${
+                        task.is_done ? "btn-success" : "btn-warning"
+                      }`}
+                      onClick={() => doneUpdate(task)}
+                    >
+                      {task.is_done ? "Terminada" : "Pendiente de hacer "}
+                    </button>
+                    <button
+                      type="button"
+                      className="btn btn-danger delete"
+                      onClick={() => deleteTask(task.id)}
+                    >
+                      Borrar
+                    </button>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
 
+          <div className="card.footer text-white footer">
+            Llevas {tasks.length} / 10 tareas agregadas a tu lista.
+            <button
+              type="button"
+              className="btn btn-danger delete"
+              onClick={() => deleteAllTasks(tasks.id)}
+            >
+              Borrar todo
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <button
+        type="button"
+        className="btn btn-danger delete"
+        onClick={() => createUser()}
+      >
+        Crear Usuario
+      </button>
+    </div>
+  );
+};
+
+export { ToDoList };
